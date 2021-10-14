f6b68d327ebadf92e0ab20ecafaf60acf20bc113
amar99587 <72984889+amar99587@users.noreply.github.com>
2021-10-14 18:51:25 +0100

    Update how.vue

--- how.vue
+++ how.vue
@@ -1,22 +1,12 @@
 <template>
-
     <div class="p-4 grid row-4 gap-4">
-    
       <div v-for="array in array" :key="array.id" class="p-2 border-2 rounded-lg">
-      
         <h1>{{array.name}}</h1>
-        
         <h1>age : {{array.age}}</h1>
-        
       </div>
-      
     </div>
-    
 </template>
-
 <script>
-
-
 export default {
   name: "How",
   components: {},
