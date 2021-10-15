ae42b2014be3e9e1b48c13f19d7d264bf1bf91e7
Salim Djerbouh <caddydz4@gmail.com>
2021-10-15 13:04:32 +0200

    A generic commit message
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- how.vue
+++ how.vue
@@ -1,46 +1,41 @@
 <template>
-    <div class="p-4 grid row-4 gap-4">
-      <div v-for="array in array" :key="array.id" class="p-2 border-2 rounded-lg">
-        <h1>{{array.name}}</h1>
-        <h1>age : {{array.age}}</h1>
-      </div>
+  <div class="p-4 grid row-4 gap-4">
+    <div v-for="array in array" :key="array.id" class="p-2 border-2 rounded-lg">
+      <h1>{{ array.name }}</h1>
+      <h1>age: {{ array.age }}</h1>
     </div>
+  </div>
 </template>
 <script>
 export default {
   name: "How",
-  components: {},
-  data() {
-    return {
-      array : [
-        {
-          name : "Adam",
-          age : 19,
-        },
-        {
-          name : "Amar",
-          age : 22
-        },
-        {
-          name : "Asma",
-          age : 16
-        },
-        {
-          name : "Nadia",
-          age : 14,
-        },
-        {
-          name : "Oussama",
-          age : 18
-        },
-        {
-          name : "Lina",
-          age : 26
-        },
-      ]
-    };
-  },
-  methods: {},
-  computed:{},
+  data: () => ({
+    array: [
+      {
+        name: "Adam",
+        age: 19,
+      },
+      {
+        name: "Amar",
+        age: 22,
+      },
+      {
+        name: "Asma",
+        age: 16,
+      },
+      {
+        name: "Nadia",
+        age: 14,
+      },
+      {
+        name: "Oussama",
+        age: 18,
+      },
+      {
+        name: "Lina",
+        age: 26,
+      },
+    ],
+  }),
 };
 </script>
